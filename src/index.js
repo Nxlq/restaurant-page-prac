91f7b387292005366042444fcf4f29c18605b0fe
JeffW <116536323+Nxlq@users.noreply.github.com>
2023-03-10 14:17:08 -0500

    add page load function to generate/render elements on screen

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import { generateHero } from "./hero-dom.js";
-generateHero();
+import { generateAboutUs } from "./about-us-dom.js";
+import { generateNav } from "./nav-dom.js";
+
+window.addEventListener("load", (e) => {
+  const contentContainer = document.querySelector("#content");
+  const main = document.querySelector("main");
+  const nav = generateNav();
+  const [heroLeft, heroRight] = generateHero();
+  console.log(heroLeft, heroRight);
+  const aboutUs = generateAboutUs();
+
+  contentContainer.insertBefore(nav, main);
+  main.append(heroLeft, heroRight);
+});
